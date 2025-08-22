b7e498ccde4e46e5294ba40ed210b28f762db57c
fabianlxf <164947673+fabianlxf@users.noreply.github.com>
2025-08-22 19:33:52 +0200

    Fix missing environment variables

--- generateVapid.js
+++
@@ -1,6 +0,0 @@
-const webpush = require("web-push");
-
-const keys = webpush.generateVAPIDKeys();
-console.log("Public Key:\n", keys.publicKey);
-console.log("Private Key:\n", keys.privateKey);
-VITE_VAPID_PUBLIC_KEY=BLe3iJOHwg1zf5iTDXUuLQVpXC5VE0AEIyi_g1rBuOrbPxVIe7oy5aSWM1yJ9H-aLOghUypBlonTyvfTgwvRKIk
